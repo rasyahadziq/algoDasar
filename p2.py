da90b8d02df9c19dcf956ff091568b5142e0c8cf
rasyahadziq <121235361+rasyahadziq@users.noreply.github.com>
2022-12-22 19:24:48 +0700

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,10 @@
+print ("""
+        @@@      @@@@   @@@    @   @   @@@@
+        @  @     @  @   @        @     @  @
+        @ @      @@@@     @      @     @@@@
+        @   @    @   @  @@@      @     @  @ """)
+
+
+a= input("kalimat: ")
+b= input("kalimat yang dicari: ")
+print(f"huruf {b} ada: ", a.count(b))
